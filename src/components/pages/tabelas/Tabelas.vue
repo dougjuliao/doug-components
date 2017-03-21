5c846901d586586eb7c53b4f8405df959d35c91c
Douglas Julião <douglas_juliao16@hotmail.com>
2017-03-21 17:05:39 -0300

    TABLE - Iniciando tabela

--- src/components/pages/tabelas/Tabelas.vue
+++ src/components/pages/tabelas/Tabelas.vue
@@ -1,10 +1,15 @@
 <template>
     <div>
         <h2>{{ titulo }}</h2>
+        <tbl></tbl>
     </div>
 </template>
 <script>
+import Table from '../../../components/shared/table/Table.vue';
 export default {
+    components: {
+        'tbl' : Table
+    },
     data(){
         return { titulo: 'Tabelas'};
     }
---
+++ src/components/shared/table/Table.vue
@@ -0,0 +1,34 @@
+<template>
+    <table>
+        <caption>{{ titulo }}</caption>
+        <thead>
+            <tr>
+                <th>Header1</th>
+                <th>Header2</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Container1</td>
+                <td>Container2</td>
+            </tr>
+        </tbody>
+        <tfoot>
+            <tr>
+                <td>Footer1</td>
+                <td>Footer2</td>
+            </tr>
+        </tfoot>
+    </table>
+</template>
+<script>
+export default {
+   props: {
+       titulo: {
+           type: String
+       }
+   }
+}
+</script>
+<style>
+</style>
